a6290031892074aa14a5d950d242a0d9545489bd
matbrady <mat.brady@threespot.com>
2013-04-15 22:00:06 -0400

    claiming vm now working

--- vmcheckout.php
+++ vmcheckout.php
@@ -20,19 +20,23 @@ class VMC extends Workflows {
 	protected $checkout_name;
 	protected $tasks = array(  // comma placement is for easier commenting of code during development
 		'claim' => array(
-			"task" => "Claim",
+			"task" => "claim",
+			"title" => "Claim",
 			"subtitle" => "Claim a Virtual Machine"
 		)
 		,'vacate' => array(
-			"task" => "Vacate",
+			"task" => "vacate",
+			"title" => "Vacate",
 			"subtitle" => "Vacate a Claimed Virtual Machine"
 		)
 		,'reset_name' => array(
-			"task" => "Reset Name",
+			"task" => "set",
+			"title" => "Reset Name",
 			"subtitle" => "Reset Your Checkout Name"
 		)
 		,'set_name' => array(
-			"task" => "Set Name",
+			"task" => "set",
+			"title" => "Set Name",
 			"subtitle" => "Set Your Checkout Name"
 		)
 		// ,'clearvm' => array(
@@ -119,8 +123,8 @@ class VMC extends Workflows {
 			* In other words: Only display "Set Name" if no name is set. Only
 			* display "Reset Name" if a name is already set 
 			*/
-			if ( $this->is_name_set() !== false && $func['task'] === "Set Name"  ||
-				 $this->is_name_set() === false && $func['task'] === "Reset Name" ) 
+			if ( $this->is_name_set() !== false && $func['title'] === "Set Name"  ||
+				 $this->is_name_set() === false && $func['title'] === "Reset Name" ) 
 			{
 				continue; // don't make a result
 			}
@@ -131,7 +135,7 @@ class VMC extends Workflows {
 				if ( preg_match( $this->pattern, $func['task'], $matches) || $query === "" ) {
 
 					// Create a result with the Task name and subtitle
-					$this->result( 'demo', $func['task'], $func['task'], $func['subtitle'], 'icon.png', 'yes' );
+					$this->result( 'demo', $func['task'], $func['title'], $func['subtitle'], 'icon.png', 'yes' );
 
 				}
 
@@ -160,12 +164,23 @@ class VMC extends Workflows {
 
 		// Name is set
 		if ( $this->is_name_set() !== false ) {
+
+			// set the search pattern
+			$this->set_match_pattern( $query );
 			
+			return $this->search_available_vms();
+
 		}
+
 		else {
 
+			// Create a Prompt Response for Alfred to display in the results
 			$prompt = array(
-				"data" => $this->query,
+				"data" => array(
+						'query' => $this->query,
+						'action' => 'set_name',
+						'message' => 'Checkout name set to: '. $this->query
+					),
 				"title" => "Set Name: ".$this->query,
 				"subtitle" => "Enter a name to be used to checkout a VM",
 				"image" => "icon.png",
@@ -173,6 +188,102 @@ class VMC extends Workflows {
 
 			return $this->prompt_user( $prompt );
 		}
+	}
+
+
+	/**
+	* Request Checkout Name Change
+	*
+	* Description: Allow users to override their existing VM Checkout Name
+	* Additions: check for any vms that have been checked out using the old name
+	* reset them to the new name
+	* @param 'string' : user input or query
+	* @return 'string' : message to be 
+	*/
+	public function request_name_change( $query ) {
+
+		$this->query = $query;
+
+		if ( $this->is_name_set() === false ) {
+
+			// Create a Prompt Response for Alfred to display in the results
+			$prompt = array(
+				"data" => array(
+						'query' => $this->query,
+						'action' => 'set_name',
+						'message' => 'Checkout name set to: '. $this->query
+					),
+				"title" => "Set Name: ".$this->query,
+				"subtitle" => "Enter a name to be used to checkout a VM",
+				"image" => "icon.png",
+				);
+
+		}
+		else {
+
+			// Create a Prompt Response for Alfred to display in the results
+			$prompt = array(
+				"data" => array(
+						'query' => $this->query,
+						'action' => 'set_name',
+						'message' => 'Checkout name set to: '. $this->query
+					),
+				"title" => "Reset Name: ".$this->query,
+				"subtitle" => "Change your name to be used to checkout a VM",
+				"image" => "icon.png",
+				);
+		}
+
+		return $this->prompt_user( $prompt );
+	}
+
+	/**
+	* Search Server for Available Virtual Machines
+	*
+	* @param 'string' : user input or query
+	* @return XML : Available VM results
+	*/
+	protected function search_available_vms() {
+
+		$url = "http://vm-checkout.threespot.dev/vm.php";
+
+		$all_vm_data = $this->fetch_all_data( $url );
+
+		foreach( $all_vm_data as $index => $vm ) {
+
+			if ( isset($vm->user) &&  $vm->user === "" && ( preg_match( $this->pattern, $vm->vm, $matches) || $this->query === "")  ) {
+
+				$vm->task = "claim";
+				$vm->url = $url;
+				$vm->action = "claim_vm";
+				$vm->name = $this->get_checkout_name();
+
+				$this->result( $index , json_encode($vm) , $vm->vm, "Checkout Virtual Machine ".$vm->vm, 'icon.png', 'yes' );
+			}
+		}
+
+		$results = $this->results();
+
+		// if ( count( $results ) == 0 ) {
+		// 	self::$wf->result( 'googlesuggest', self::$query, 'No Suggestions', 'No search suggestions found. Search Google for '.self::$query, 'icon.png' );
+		// }
+
+		return $this->toxml();
+
+	}
+
+	/**
+	* Gets the enitre list of VMS from the server
+	* set the data request url
+	*
+	* @param 'string' : url to request data from
+	* @return array of all the VMS
+	*/
+	protected function fetch_all_data( $url = "http://vm-checkout.threespot.dev/vm.php" ) {
+
+		$data = file_get_contents( $url );
+
+		return json_decode( $data );
 	}
 
 	/**
@@ -185,15 +296,10 @@ class VMC extends Workflows {
 	*/
 	protected function prompt_user( $prompt ) {
 
-		$this->result( 'demo', $prompt['data'], $prompt['title'], $prompt['subtitle'], $prompt['image'], 'yes' );
+		$this->result( 'demo', json_encode($prompt['data']), $prompt['title'], $prompt['subtitle'], $prompt['image'], 'yes' );
 
 		return $this->toxml();
-
 	}
-
-	/**
-	*/
-	public function run_task() {}
 
 	/**
 	* Set the Name used to Claim and Vacate a VM listing
@@ -202,11 +308,102 @@ class VMC extends Workflows {
 
 	}
 
+	protected function get_checkout_name() {
+
+		return file_get_contents( 'name.txt' );
+	}
+
+	/**
+	* 
+	*/
+	public function notify_user( $passed_data ) {
+
+		$data = json_decode($passed_data);
+
+		switch (  $data->action ) {
+
+			case 'set_name':
+				file_put_contents( 'name.txt', $data->query );
+				return $data->message;
+			break;
+
+			case 'claim_vm':
+
+			break;
+
+			default: 
+				return '';
+			break;
+		}
+
+	}
+
 	/**
 	* Claim an Available Virtual Machine
+	*
+	* Description: Checks for the claim_vm action, 
+	* sends a PUT request to claim a VM from the server,
+	* notifies the user of successful claim.
+	* @param OBJECT : data passed from vm results
+	* @return 'string' : response from CURL command
 	*/	
-	protected function claim_vm() {
+	public function claim_vm( $passed_data ) {
 
+		$data = json_decode($passed_data);
+
+		if ( $data->action === 'claim_vm' ) {
+
+			return $this->send_vm_claim_request( $data );
+		}
+
+		else return '';
+
+	}
+
+	/**
+	* Send Claim Request to Server
+	*
+	* Description: Creates a JSON string and sends a PUT 
+	* request to ther server for claiming a VM
+	* @param Object : data used to generate curl command
+	* @return 'string' : message to user
+	*/
+	protected function send_vm_claim_request( $data ) {
+
+		date_default_timezone_set('America/New_York');
+		$date = date("Y-m-d H:i:s");
+
+		$update_json = '{"id":"'.$data->id.'","user":"'.$data->name.'","checkout":"'.$date.'"}';
+
+		$chlead = curl_init();
+
+		// set URL and other appropriate options
+		$options = array(
+			CURLOPT_URL => $data->url,
+		  	CURLOPT_HTTPHEADER => array('Content-Type: application/json','Content-Length: ' . strlen( $update_json ) ),
+		  	CURLOPT_VERBOSE => 1,
+		  	CURLOPT_RETURNTRANSFER => true,
+		  	CURLOPT_CUSTOMREQUEST => "PUT",
+		  	CURLOPT_POSTFIELDS => $update_json,
+		  	CURLOPT_SSL_VERIFYPEER => 0,
+		);
+
+		curl_setopt_array($chlead, $options);
+
+		$chleadresult = curl_exec($chlead);
+		$chleadapierr = curl_errno($chlead);
+		$chleaderrmsg = curl_error($chlead);
+		curl_close($chlead);
+
+		if ( $chleadapierr === 1 ) {
+		  	return $chleadapierr;
+		}
+		else if ( $chleaderrmsg === 1 ) {
+			return $chleaderrmsg;
+		}
+		else {
+		  return 'You now own '.$data->vm;
+		}
 	}
 
 	/**
